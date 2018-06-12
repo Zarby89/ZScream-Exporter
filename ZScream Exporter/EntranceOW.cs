029428f705a6d9587413a91820596c50b59b2b78
Zarby89 <adamoadamo12323@hotmail.com>
2018-06-11 20:58:27 -0400

    new Repo

---
+++ ZScream Exporter/EntranceOW.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeldaFullEditor
+{
+    public struct EntranceOW
+    {
+        public short mapPos;
+        public short mapId;
+        public byte entranceId;
+        public EntranceOW(short mapId, short mapPos, byte entranceId)
+        {
+            this.mapPos = mapPos;
+            this.mapId = mapId;
+            this.entranceId = entranceId;
+           
+        }
+
+    }
+}
--- ZScream Exporter/Program.cs
+++ ZScream Exporter/Program.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ZScream_Exporter
+namespace ZeldaFullEditor
 {
     static class Program
     {
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new zscreamForm());
         }
     }
 }
